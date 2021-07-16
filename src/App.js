b7e43bda65e28b310b335eca984692df67cd2dc5
Christopher Atoki <christopheratoki@Christophers-MacBook-Pro.local>
2021-07-15 20:40:22 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  // sync up with, if any. Beware infinite loops!
+  const [characters, setCharacters] = useState([]);
+  const [films] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((resp) => {
+        setCharacters(resp.data);
+      })
+      .catch(() => {
+        console.log("server error");
+      });
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">
+        {characters.map((c) => (
+          <Character
+            data-testid="character"
+            key={c.name}
+            character={c}
+            films={films}
+          />
+        ))}
+      </h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Films.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Film from "./Film";
+
+const Films = ({ films }) => {
+  return (
+    <>
+      {films.map((f) => (
+        <Film key={f} filmURL={f} />
+      ))}
+    </>
+  );
+};
+
+export default Films;
